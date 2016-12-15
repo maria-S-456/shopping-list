8437b89bc51e2fb424ad0512585e7f88eb2a0182
maria-S-456 <mars381pika@gmail.com>
2016-12-15 11:00:59 -0500

    second commit

--- js-shopping-list.js
+++ js-shopping-list.js
@@ -5,7 +5,7 @@ var addItem = function(state, item)
 }
 var displayItem = function(state, element){
 	var htmlItems = state.items.map(function(item){
-		return '<li class="box">' + item + '</br><button class="divBtns" id="checkBtn">Check</button><button class="divBtns" id="deleteBtn">Delete</button>' + '</li>'; 
+		return '<li class="box" id="listBox">' + item + '</br><button class="divBtns" id="checkBtn">Check</button><button class="divBtns" id="deleteBtn">Delete</button>' + '</li>'; 
 	});
 	element.html(htmlItems);
 }
@@ -15,7 +15,7 @@ $('.shopping-list-add').submit(function(event){
 	addItem(state, $('.shopping-list-add-input').val());
 	displayItem(state, $('.shopping-list') );
 	/* alert(state.items[1]);			shows that the items array holds everything that is turned into a div*/ 
-})
+});
 
 
 
@@ -27,14 +27,20 @@ $(document).ready(function(){
 })
 
 */
-
+	
 
 
 	$(document).ready(function(e){ 				
-		$('ul').on('click', '#deleteBtn', function(event){
-			var rmvButton = $(this).closest('li');
-			rmvButton.remove();
 		
-		});
 		
+			$('ul').on('click', '#deleteBtn', function(event){
+				var index = $(this).data('index');
+				state.items.splice(index,1); //NEW
+				$(this).closest("li").remove();
+			});
+
+			$('ul').on('click', '#checkBtn', function(event){
+				$(this).closest("li").toggleClass("line-through");
+			});
 	})
+	
